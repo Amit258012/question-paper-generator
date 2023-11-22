2e2e933f94b6b97d7ffab05372364d53cd453a46
Amit258012 <amit258012@gmail.com>
2023-11-22 09:10:05 +0530

    padding updated new feature branch : I want to update the marks of any difficulty to any marks vaue, because the 1marks question can be hard and so on..

---
+++ src/userInput.js
@@ -0,0 +1,71 @@
+// const readline = require("readline");
+// const difficultyLevels = ["easy", "medium", "hard"];
+// const difficultyRatios = {};
+
+// let totalMarks; // Declare totalMarks at a higher scope
+// FIXME : difficulty ratio is not working it is returning {}
+
+// function getUserInput() {
+// 	const rl = readline.createInterface({
+// 		input: process.stdin,
+// 		output: process.stdout,
+// 	});
+
+// 	// Function to recursively get input for each difficulty level
+// 	function getDifficultyRatio(index) {
+// 		if (index < difficultyLevels.length) {
+// 			const difficultyLevel = difficultyLevels[index];
+// 			rl.question(
+// 				`Enter difficulty ratio for ${difficultyLevel}: `,
+// 				(ratioInput) => {
+// 					const ratio = parseFloat(ratioInput);
+// 					if (isNaN(ratio)) {
+// 						console.error(
+// 							"Invalid input. Please enter a valid numeric value."
+// 						);
+// 						getDifficultyRatio(index); // Ask the same question again
+// 					} else {
+// 						difficultyRatios[difficultyLevel] = ratio;
+// 						getDifficultyRatio(index + 1); // Move to the next difficulty level
+// 					}
+// 				}
+// 			);
+// 		} else {
+// 			// All difficulty ratios collected, process the input
+// 			const userInput = {
+// 				totalMarks,
+// 				difficultyRatios,
+// 			};
+// 			console.log("User input:", userInput);
+// 			rl.close();
+// 		}
+// 	}
+
+// 	// Ask the user for the total marks
+// 	rl.question("Enter the total marks: ", (totalMarksInput) => {
+// 		totalMarks = parseInt(totalMarksInput);
+
+// 		if (isNaN(totalMarks)) {
+// 			console.error("Invalid input. Please enter a valid numeric value.");
+// 			rl.close();
+// 			return;
+// 		}
+
+// 		// Start getting input for each difficulty level
+// 		getDifficultyRatio(0);
+// 	});
+
+// 	console.log("Waiting for user input...");
+// }
+
+// // Exporting totalMarks and difficultyRatios for use in other parts of the project
+// module.exports = {
+// 	getUserInput,
+// 	totalMarks,
+// 	difficultyRatios,
+// };
+
+const totalMarks = 50;
+const difficultyRatios = { Easy: 0.3, Medium: 0.4, Hard: 0.3 };
+
+module.exports = { totalMarks, difficultyRatios };
