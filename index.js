6adfc0be30a94e50d001794cb1036c73b59879b7
Amit258012 <amit258012@gmail.com>
2023-11-22 09:04:18 +0530

    created reuable function (getQuestions)

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-// const { questionStore } = require("./src/services/questionStore");
 const generateQuestionPaper = require("./src/services/questionPaperGenerator");
+const { questionStore } = require("./src/services/questionStore");
+const { totalMarks, difficultyRatios } = require("./src/userInput");
+const { getQuestions } = require("./src/utils/getQuestions");
+
+// FIXME : User input not working
+// const {
+// 	totalMarks,
+// 	difficultyRatios,
+// 	getUserInput,
+// } = require("./src/userInput");
+
+// getUserInput();
 
 // Display all loaded questions
-console.log("All Loaded Questions:");
-// questionStore.forEach((question, index) => {
-// 	console.log(
-// 		`${index + 1}. ${question.question} - Difficulty: ${
-// 			question.difficulty
-// 		} - Marks: ${question.marks}`
-// 	);
-// });
-
-// TODO: in main file just call functions
-// TODO: Create Input file and take the values from there
-// TODO: How to distribute questions correctly
-// TODO: Handle edge cases
-// TODO: Think of this, easy question can carry marks =[1..5] and medium question marks =[6...12] and hard question marks = [13...20]
+getQuestions(questionStore);
 
 // Generate a question paper
-const totalMarks = 50;
-const difficultyDistribution = { Easy: 0.3, Medium: 0.4, Hard: 0.3 };
-const questionPaper = generateQuestionPaper(totalMarks, difficultyDistribution);
+const questionPaper = generateQuestionPaper(totalMarks, difficultyRatios);
 
 // Log the generated question paper
 console.log("\nGenerated Question Paper:");
-questionPaper.forEach((question, index) => {
-	console.log(
-		`${index + 1}. ${question.question}     ( Difficulty: ${
-			question.difficulty
-		} ) - ( Marks: ${question.marks} )`
-	);
-});
+getQuestions(questionPaper);
---
+++ src/utils/getQuestions.js
@@ -0,0 +1,20 @@
+function getQuestions(questionStore) {
+	const maxDifficultyLength = Math.max(
+		...questionStore.map((q) => q.difficulty.length)
+	);
+	const maxMarksLength = Math.max(
+		...questionStore.map((q) => q.marks.toString().length)
+	);
+	questionStore.forEach((question, index) => {
+		console.log(
+			`${index + 1}. ${question.question.padEnd(
+				50
+			)} ( Difficulty: ${question.difficulty.padEnd(
+				maxDifficultyLength
+			)} ) - ( Marks: ${question.marks
+				.toString()
+				.padEnd(maxMarksLength)} )`
+		);
+	});
+}
+module.exports = { getQuestions };
